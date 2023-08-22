83a663d5fd52beb4fb8c3cdaf4796417d6bad2df
Jade Ornelas <jade@ornelas.io>
2023-08-21 17:27:11 -0700

    snippet to parse data from meetup page

---
+++ parseMeetup.js
@@ -0,0 +1,11 @@
+var data = document.querySelectorAll('[data-event-label="attendee-card"]')
+var res = []
+data.forEach(function (item) {
+  // find children by class name
+  var name = item.getElementsByClassName('font-medium')[0].innerText
+  // find image if exists by tag name and get src attribute
+  var image = item.getElementsByTagName('img')[0] ? item.getElementsByTagName('img')[0].getAttribute('src') : ''
+  res.push({name, image})
+})
+
+console.log(JSON.stringify(res))
